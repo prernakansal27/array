4383e30514adb517ccaf1cc1846d981b572f56cf
prernakansal27 <96967316+prernakansal27@users.noreply.github.com>
2023-09-09 15:39:15 +0530

    min_max.cpp

---
+++ min_max.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+//3.MINIMUM AND MAXIUM VALUE IN ARRAY//
+int main() {
+  //MINIMUM VALUE
+  //2 TO THE POWE 31
+  cout<<INT_MIN<<endl;
+  //MAXIMUM VALUE
+  //2 to the power 31-1
+  cout<<INT_MAX<<endl;
+  cout << "Hello world!" << endl;
+  return 0;
+}
